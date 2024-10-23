97ca5b966d2c38a6c37eb2825904eef624e9c322
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-10-23 14:38:15 +0200

    ADD prompt and discount

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,22 @@
 console.log('js-biglietto-treno-form');
+
+const age = prompt("Inserisci la tua età?")
+const kilometres = prompt("Inserisci chilometri da percorrere?")
+const ticketPriceForKilometres = 0.21
+const basicTicketPrice = kilometres * ticketPriceForKilometres
+console.log ('basicTicketPrice:' , basicTicketPrice)
+
+let discount
+
+if (age < 17) {
+	discount = basicTicketPrice * (20 / 100)
+} else if (age > 65) {
+	discount = basicTicketPrice * (40 / 100)
+} else {
+	discount = 0
+}
+
+console.log ('discount:' , discount)
+
+const ticketPrice = basicTicketPrice - discount
+console.log ('ticketPrice:' , ticketPrice.toFixed(2))
